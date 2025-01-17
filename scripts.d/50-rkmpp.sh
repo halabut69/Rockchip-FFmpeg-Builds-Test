MarcA711/Rockchip-FFmpeg-Builds

add rockchip build

scripts.d/50-rkmpp.sh
@@ -1,0 +1,38 @@
+#!/bin/bash
+
+# this is the upstream repo of mpp
+# SCRIPT_REPO="https://github.com/rockchip-linux/mpp.git"
+# SCRIPT_COMMIT="bebc9961103af2b53fb18175dd858b15a73c9ad8"
+
+# this is a fork from nyanmisaka with some additional fixes
+SCRIPT_REPO="https://github.com/nyanmisaka/mpp.git"
+SCRIPT_COMMIT="d1078ad2b69dadd405457543833c0f894bd82523"
+SCRIPT_BRANCH="jellyfin-mpp"
+
+ffbuild_enabled() {
+    [[ $ADDINS_STR == *-rk ]] && return 0
+    return -1
+}
+
+ffbuild_dockerbuild() {
+    mkdir bld
+    cd bld
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TEST=OFF -DBUILD_SHARED_LIBS=OFF ..
+    make -j$(nproc)
+    make install
+
+    rm -rf $FFBUILD_PREFIX/lib/librockchip_mpp.so*
+}
+
+ffbuild_configure() {
+    echo --enable-rkmpp
+}
+
+ffbuild_unconfigure() {
+    echo --disable-rkmpp
+}
+
+ffbuild_libs() {
+    echo -lstdc++
+}

scripts.d/50-rkrga.sh
@@ -1,0 +1,53 @@
+#!/bin/bash
+
+# this is the upstream repo of mpp
+# SCRIPT_REPO="https://github.com/JeffyCN/mirrors.git"
+# SCRIPT_COMMIT="d7a0a485ed6c201f882c20b3a8881e801f131385"
+# SCRIPT_BRANCH="linux-rga-multi"
+
+# this is a fork from nyanmisaka with some additional fixes
+SCRIPT_REPO="https://github.com/nyanmisaka/rk-mirrors.git"
+SCRIPT_COMMIT="a9fc19e6b906d7cecd6bcefbd45e5e151831d33f"
+SCRIPT_BRANCH="jellyfin-rga"
+
+ffbuild_enabled() {
+    [[ $ADDINS_STR == *-rk ]] && return 0
+    return -1
+}
+
+ffbuild_dockerbuild() {
+    sed -i 's/shared_library/library/g' meson.build
+
+    mkdir builddir && cd builddir
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --buildtype=release
+        --default-library=static
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    meson "${myconf[@]}" ..
+    ninja -j$(nproc)
+    ninja install
+}
+
+ffbuild_configure() {
+    echo --enable-rkrga
+}
+
+ffbuild_unconfigure() {
+    echo --enable-rkrga
+}
+
+ffbuild_libs() {
+    echo -lstdc++
+}

halabut69/Rockchip-FFmpeg-Builds-Test

scripts.d/50-rkmpp.sh
@@ -1,0 +1,38 @@
+#!/bin/bash
+
+# this is the upstream repo of mpp
+# SCRIPT_REPO="https://github.com/rockchip-linux/mpp.git"
+# SCRIPT_COMMIT="bebc9961103af2b53fb18175dd858b15a73c9ad8"
+
+# this is a fork from nyanmisaka with some additional fixes
+SCRIPT_REPO="https://github.com/nyanmisaka/mpp.git"
+SCRIPT_COMMIT="fda5a02e8f88e79ef110e7912c20326a1fea51fb"
+SCRIPT_BRANCH="jellyfin-mpp"
+
+ffbuild_enabled() {
+    [[ $ADDINS_STR == *-rk ]] && return 0
+    return -1
+}
+
+ffbuild_dockerbuild() {
+    mkdir bld
+    cd bld
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TEST=OFF -DBUILD_SHARED_LIBS=OFF ..
+    make -j$(nproc)
+    make install
+
+    rm -rf $FFBUILD_PREFIX/lib/librockchip_mpp.so*
+}
+
+ffbuild_configure() {
+    echo --enable-rkmpp
+}
+
+ffbuild_unconfigure() {
+    echo --disable-rkmpp
+}
+
+ffbuild_libs() {
+    echo -lstdc++
+}

scripts.d/50-rkrga.sh
@@ -1,0 +1,53 @@
+#!/bin/bash
+
+# this is the upstream repo of mpp
+# SCRIPT_REPO="https://github.com/JeffyCN/mirrors.git"
+# SCRIPT_COMMIT="d7a0a485ed6c201f882c20b3a8881e801f131385"
+# SCRIPT_BRANCH="linux-rga-multi"
+
+# this is a fork from nyanmisaka with some additional fixes
+SCRIPT_REPO="https://github.com/nyanmisaka/rk-mirrors.git"
+SCRIPT_COMMIT="e97e327662d1c0867d4cbcc01d5caf15250fc4e7"
+SCRIPT_BRANCH="jellyfin-rga"
+
+ffbuild_enabled() {
+    [[ $ADDINS_STR == *-rk ]] && return 0
+    return -1
+}
+
+ffbuild_dockerbuild() {
+    sed -i 's/shared_library/library/g' meson.build
+
+    mkdir builddir && cd builddir
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --buildtype=release
+        --default-library=static
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    meson "${myconf[@]}" ..
+    ninja -j$(nproc)
+    ninja install
+}
+
+ffbuild_configure() {
+    echo --enable-rkrga
+}
+
+ffbuild_unconfigure() {
+    echo --enable-rkrga
+}
+
+ffbuild_libs() {
+    echo -lstdc++
+}