fee6070fe4ecf0927252a03597a919868035fc1a
halabut69 <155487809+halabut69@users.noreply.github.com>
2025-03-05 10:08:01 +0100

    debugging

--- scripts.d/50-rkmpp.sh
+++ scripts.d/50-rkmpp.sh
@@ -22,6 +22,9 @@ ffbuild_dockerbuild() {
     mkdir bld
     cd bld
 
+    # Ensure that the PKG_CONFIG_PATH is set to the directory containing rockchip_mpp.pc
+    export PKG_CONFIG_PATH="$FFBUILD_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TEST=OFF -DBUILD_SHARED_LIBS=OFF ..
     make -j$(nproc)
     make install
