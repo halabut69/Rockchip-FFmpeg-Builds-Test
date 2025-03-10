b01b4b627173391114c1a554cffbb2114d2dded9
halabut69 <155487809+halabut69@users.noreply.github.com>
2025-03-10 17:55:07 +0100

    remove disable debug

--- scripts.d/50-rkmpp.sh
+++ scripts.d/50-rkmpp.sh
@@ -18,8 +18,13 @@ ffbuild_dockerbuild() {
     mkdir bld
     cd bld
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TEST=OFF -DBUILD_SHARED_LIBS=OFF ..
-    make -j$(nproc)
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+      -DCMAKE_BUILD_TYPE=Debug \  # Use Debug build
+      -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+      -DBUILD_TEST=OFF \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_C_FLAGS="-g -O0" \  # Ensure debug symbols
+      -DCMAKE_CXX_FLAGS="-g -O0" ..      make -j$(nproc)
     make install
 
     rm -rf $FFBUILD_PREFIX/lib/librockchip_mpp.so*
